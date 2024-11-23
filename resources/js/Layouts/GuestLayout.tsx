065ed38cfbb2a38fab9626791f2da14b694ab408
Franco Sanllehi <francosanllehi@gmail.com>
2024-11-23 13:15:50 -0300

    feat: application logo

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -14,7 +14,7 @@ export default function GuestLayout({
                         <div className="flex">
                             <div className="flex shrink-0 items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <ApplicationLogo />
                                 </Link>
                             </div>
                         </div>
