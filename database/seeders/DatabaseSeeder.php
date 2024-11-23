ce2698d9ab6f4c6abce87db87381570d3630479e
Franco Sanllehi <francosanllehi@gmail.com>
2024-11-23 16:44:32 -0300

    feat: community seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Community;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        try {
+            $community = Community::factory()->create([
+                'name' => 'Switch 345',
+                'slug' => 'switch-345',
+            ]);
+        } catch (\Exception $e) {
+            dump('Error creating community:', $e->getMessage());
+        }
     }
 }
